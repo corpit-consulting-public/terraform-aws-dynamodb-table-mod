c8f36686e38cc9fd54df69ff00594ac1a7b31a87
Nicolas Martinez <nmartinez@corpit-consulting.com>
2019-05-06 21:48:17 -0300

    New output arn

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "Table name"
   value       = "${aws_dynamodb_table.basic-dynamodb-table.id}"
 }
+
+output "arn" {
+  description = "Table ARN"
+  value       = "${aws_dynamodb_table.basic-dynamodb-table.arn}"
+}
