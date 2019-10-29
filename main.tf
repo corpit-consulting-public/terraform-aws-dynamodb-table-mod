524e39f87e8b34a8ce099a8edefdcd12e501e0ea
Nicolas Martinez <nmartinez@corpit-consulting.com>
2019-10-29 14:35:33 -0300

    Support for terraform 12

--- main.tf
+++ main.tf
@@ -1,80 +1,51 @@
-resource "aws_dynamodb_table" "dynamodb-table-1" {
-  count            = "${ !var.has_global_secondary_index && !var.has_local_secondary_index && var.billing_mode == "PROVISIONED" ? 1 : 0}"
-  name             = "${var.name}"
-  billing_mode     = "${var.billing_mode}"
-  read_capacity    = "${var.read_capacity}"
-  write_capacity   = "${var.write_capacity}"
-  hash_key         = "${var.hash_key}"
-  range_key        = "${var.range_key}"
-  attribute        = "${var.attribute}"
-  stream_enabled   = "${var.stream_enabled}"
-  stream_view_type = "${var.stream_view_type}"
-} 
+resource "aws_dynamodb_table" "dynamodb-table" {
+  name           = var.name
+  billing_mode   = var.billing_mode
+  hash_key       = var.hash_key
+  range_key      = var.range_key
+  dynamic "attribute" {
+    for_each = var.attribute
+    content {
+      # TF-UPGRADE-TODO: The automatic upgrade tool can't predict
+      # which keys might be set in maps assigned here, so it has
+      # produced a comprehensive set here. Consider simplifying
+      # this after confirming which keys can be set in practice.
 
-resource "aws_dynamodb_table" "dynamodb-table-1_on_demand" {
-  count            = "${ !var.has_global_secondary_index && !var.has_local_secondary_index && var.billing_mode != "PROVISIONED" ? 1 : 0}"
-  name             = "${var.name}"
-  billing_mode     = "${var.billing_mode}"
-  hash_key         = "${var.hash_key}"
-  range_key        = "${var.range_key}"
-  attribute        = "${var.attribute}"
-  stream_enabled   = "${var.stream_enabled}"
-  stream_view_type = "${var.stream_view_type}"
-} 
-
-resource "aws_dynamodb_table" "dynamodb-table-2" {
-  count            = "${ var.has_global_secondary_index && !var.has_local_secondary_index ? 1 : 0}"
-  name             = "${var.name}"
-  billing_mode     = "${var.billing_mode}"
-  read_capacity    = "${var.read_capacity}"
-  write_capacity   = "${var.write_capacity}"
-  hash_key         = "${var.hash_key}"
-  range_key        = "${var.range_key}"
-  attribute        = "${var.attribute}"
-  stream_enabled   = "${var.stream_enabled}"
-  stream_view_type = "${var.stream_view_type}"
-  global_secondary_index {
-    name               = "${var.global_index_name}"
-    hash_key           = "${var.global_index_hash_key}"
-    range_key          = "${var.global_index_range_key}"
-    write_capacity     = "${var.global_index_write_capacity}"
-    read_capacity      = "${var.global_index_read_capacity}"
-    projection_type    = "${var.global_index_projection_type}"
-    non_key_attributes = "${var.global_index_non_key_attributes}"
+      name = attribute.value.name
+      type = attribute.value.type
+    }
   }
-  tags = "${var.tags}"
-}
 
-resource "aws_dynamodb_table" "dynamodb-table-3" {
-  count            = "${ ! var.has_global_secondary_index && var.has_local_secondary_index ? 1 : 0}"
-  name             = "${var.name}"
-  billing_mode     = "${var.billing_mode}"
-  read_capacity    = "${var.read_capacity}"
-  write_capacity   = "${var.write_capacity}"
-  hash_key         = "${var.hash_key}"
-  range_key        = "${var.range_key}"
-  attribute        = "${var.attribute}"
-  stream_enabled   = "${var.stream_enabled}"
-  stream_view_type = "${var.stream_view_type}"
-  local_secondary_index {
-    name               = "${var.index_name}"
-    range_key          = "${var.index_range_key}"
-    projection_type    = "${var.projection_type}"
-    non_key_attributes = "${var.non_key_attributes}"
+  stream_enabled   = var.stream_enabled
+  stream_view_type = var.stream_view_type
+  tags = var.tags
+  
+  read_capacity  = var.billing_mode == "PROVISIONED" ? var.read_capacity : null
+  write_capacity = var.billing_mode == "PROVISIONED" ? var.write_capacity : null
+
+
+  dynamic "global_secondary_index" {
+      for_each = var.global_secondary_index
+
+      content {
+        name               = global_secondary_index.value.name
+        hash_key           = global_secondary_index.value.hash_key
+        projection_type    = global_secondary_index.value.projection_type
+        range_key          = lookup(global_secondary_index.value, "range_key", null)
+        non_key_attributes = lookup(global_secondary_index.value, "non_key_attributes", null)
+        read_capacity      = lookup(global_secondary_index.value, "read_capacity", null)
+        write_capacity     = lookup(global_secondary_index.value, "write_capacity", null)
+      }
   }
-  tags = "${var.tags}"
+
+  dynamic "local_secondary_index" {
+      for_each = var.local_secondary_index
+
+      content {
+        name               = local_secondary_index.value.name
+        projection_type    = local_secondary_index.value.projection_type
+        range_key          = local_secondary_index.value.range_key
+        non_key_attributes = lookup(local_secondary_index.value, "non_key_attributes", null)
+      }
+  }
 }
-
-# Notes: attribute can be lists#
-
-# attribute = [{
-   # name = "UserId"
-   # type = "S"
- # }, {
-  #  name = "GameTitle"
-   # type = "S"
- # }, {
-   # name = "TopScore"
-   # type = "N"
- # }]
-
--- output.tf
+++ output.tf
@@ -1,23 +1,18 @@
 output "id" {
   description = "Table name"
-  value = "${element(coalescelist(aws_dynamodb_table.dynamodb-table-1.*.id,aws_dynamodb_table.dynamodb-table-1_on_demand.*.id,aws_dynamodb_table.dynamodb-table-2.*.id,aws_dynamodb_table.dynamodb-table-3.*.id),0)}"
+  value       = aws_dynamodb_table.dynamodb-table.id
 }
 output "arn" {
   description = "Table ARN"
-  value       = "${element(coalescelist(aws_dynamodb_table.dynamodb-table-1.*.arn,aws_dynamodb_table.dynamodb-table-1_on_demand.*.arn,aws_dynamodb_table.dynamodb-table-2.*.arn,aws_dynamodb_table.dynamodb-table-3.*.arn),0)}"
+  value       = aws_dynamodb_table.dynamodb-table.arn
 }
 
 output "stream_arn" {
   description = "The ARN of the Table Stream. Only available when stream_enabled = true"
-  value       = "${element(coalescelist(aws_dynamodb_table.dynamodb-table-1.*.stream_arn,aws_dynamodb_table.dynamodb-table-1_on_demand.*.stream_arn,aws_dynamodb_table.dynamodb-table-2.*.stream_arn,aws_dynamodb_table.dynamodb-table-3.*.stream_arn),0)}"
+  value       = aws_dynamodb_table.dynamodb-table.stream_arn
 }
 
 output "hash_key" {
   description = "The attribute to use as the hash (partition) key. Must also be defined as an attribute."
-  value       = "${element(coalescelist(aws_dynamodb_table.dynamodb-table-1.*.hash_key,aws_dynamodb_table.dynamodb-table-1_on_demand.*.hash_key,aws_dynamodb_table.dynamodb-table-2.*.hash_key,aws_dynamodb_table.dynamodb-table-3.*.hash_key),0)}"
+  value       = aws_dynamodb_table.dynamodb-table.hash_key
 }
-
-#output "id" {
- # description = "Table name"
-#  value       = "${aws_dynamodb_table.basic-dynamodb-table.id}"
-#}
