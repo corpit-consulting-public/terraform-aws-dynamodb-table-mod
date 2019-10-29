7a755c1e5e21be929a7c800b1249c45af206ad55
Nicolas Martinez <nmartinez@corpit-consulting.com>
2019-10-29 14:54:09 -0300

    New README file

--- variables.tf
+++ variables.tf
@@ -1,35 +1,35 @@
 ##Variable for DynamoDB Table
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Tne name of the table, this needs to be unique within a region"
 }
 
 variable "billing_mode" {
-  type        = "string"
+  type        = string
   description = "Crontrols how you are charged for read and writhe throughput and how you manage capacity. The valid values are PROVISIONED and PAY_PER_REQUEST. Default to PROVISIONED."
   default     = "PROVISIONED"
 }
 
 variable "hash_key" {
-  type        = "string"
+  type        = string
   description = "The attribute to use as the hash (partition) key. Must also be defined as an attribute (Required, Forces new resource)"
 }
 
 variable "range_key" {
-  type        = "string"
+  type        = string
   description = "The attribute to use as the range (sort) key. Must also be defined as an attribute(Optional, Force new resource)"
   default     = ""
 }
 
 variable "write_capacity" {
-  type        = "string"
+  type        = string
   description = "The number of the write units for thes table. If the billing_mode is PROVISIONED, this field is required"
   default     = ""
 }
 
 variable "read_capacity" {
-  type        = "string"
+  type        = string
   description = "The number of the read units for this table. If the billing_mode is PROVISIONED, this field is required"
   default     = ""
 }
@@ -40,19 +40,19 @@ variable "attribute" {
 }
 
 variable "attribute_name" {
-  type        = "string"
+  type        = string
   description = "The name of attribute"
   default     = ""
 }
 
 variable "attribute_type" {
-  type        = "string"
+  type        = string
   description = "Attribute type, which must be a scalar type: S(string), N(number) or B(binary data)"
   default     = ""
 }
 
 variable "ttl" {
-  type        = "string"
+  type        = string
   description = "Defines ttl, has two properties, and can only be specified once:"
   default     = ""
 }
@@ -64,19 +64,19 @@ variable "ttl_enable" {
 }
 
 variable "ttl_attribute_name" {
-  type        = "string"
+  type        = string
   description = "The name of the table attribute to store the TTL timestamp in"
   default     = ""
 }
 
 variable "local_secondary_index" {
-  #type        = "string"
+  #type        = string
   description = "Describe an LSI on the table; these can only be allocated at creation so you cannot change this definition after you created the resource (Optional, Forces new resource)"
   default     = []
 }
 
 variable "global_secondary_index" {
-  #type        = "string"
+  #type        = string
   description = "Describe a GSI for the table; subject to the normal limits on the number of GSIs, projected attributes, etc"
   default     = []
 }
@@ -100,13 +100,13 @@ variable "stream_enabled" {
 }
 
 variable "stream_view_type" {
-  type        = "string"
+  type        = string
   description = "When an item in the table is modified, Tream_View_Type determines what information is writtem to the table's stream. Valid values are KEYS_ONLY, NEW_IMAGE, OLD_IMAGE, NEW_AND_OLD_IMAGES"
   default     = ""
 }
 
 variable "server_side_encryption" {
-  type        = "string"
+  type        = string
   description = "Encryption at reset options. AWS DynamoDB tables are automatically encrypted at reset whit an AWS owned Customer Master KEy if this argument isn't specified"
   default     = ""
 }
@@ -118,7 +118,7 @@ variable "tags" {
 }
 
 variable "point_in_time_recovery" {
-  type        = "string"
+  type        = string
   description = "Point-in-time recovery options"
   default     = ""
 }
@@ -127,19 +127,19 @@ variable "point_in_time_recovery" {
 ##The timeouts block allows you to specify timeouts for certain action
 
 variable "create" {
-  type        = "string"
+  type        = string
   description = "Used when creating the table (defaults to 10 mins)"
   default     = "10"
 }
 
 variable "update" {
-  type        = "string"
+  type        = string
   description = "Used when updating the table configuration and reset for each individual Global Secondary Index update (Defaults to 60 mins)"
   default     = "60"
 }
 
 variable "delete" {
-  type        = "string"
+  type        = string
   description = "Used when deleting the table (defaults to 10 mins)"
   default     = "10"
 }
@@ -148,25 +148,25 @@ variable "delete" {
 ##Variables for local_secondary_index
 
 #variable "index_name" {
-#  type        = "string"
+#  type        = string
 #  description = "The name of the index. Required if has_local_secondary_index is true"
 #  default     = ""
 #}
 
 #variable "index_hash_key" {
-#  type        = "string"
+#  type        = string
 #  description = "The name of the hash key; must be defined. Required if has_local_secondary_index is true"
 #  default     = ""
 #}
 
 #variable "index_range_key" {
-#  type        = "string"
+#  type        = string
 #  description = "The name of the range key; must be defined. Required if has_local_secondary_index is true"
 #  default     = ""
 #}
 
 #variable "projection_type" {
-#  type        = "string"
+#  type        = string
 #  description = "One of ALL, INCLUDE or KEYS_ONLY where ALL projects every attribute into the index, KEYS_ONLY projects just the hash and range key into the index, an INCLUDE projects only the keys specified the non_key_attributes parameter. Required if has_local_secondary_index is true"
 #  default     = ""
 #}
@@ -180,37 +180,37 @@ variable "delete" {
 ##Variable for global_secondary_index
 
 #variable "global_index_name" {
-#  type        = "string"
+#  type        = string
 #  description = "The name of the index. Required if has_global_secondary_index is true"
 #  default     = ""
 #}
 
 #variable "global_index_write_capacity" {
-#  type        = "string"
+#  type        = string
 #  description = "The number of write units for this index. Must be set if billing+mode is set to PROVISIONED"
 #  default     = "10"
 #}
 
 #variable "global_index_read_capacity" {
-#  type        = "string"
+#  type        = string
 #  description = "The number of read units for this index. Must be set if billing_mode is set to PROVISIONED"
 #  default     = "10"
 #}
 
 #variable "global_index_hash_key" {
-#  type        = "string"
+#  type        = string
 #  description = "The name of the hash key in the index; must be defined as an attribute in the resource. Required if has_global_secondary_index is true"
 #  default     = ""
 #}
 
 #variable "global_index_range_key" {
-#  type        = "string"
+#  type        = string
 #  description = "The name of the range key; must be defined"
 #  default     = ""
 #}
 
 #variable "global_index_projection_type" {
-#  type        = "string"
+#  type        = string
 #  description = "One of ALL, INCLUDE or KEYS_ONLY where ALL projects every attribute into the index, KEYS_ONLY project just the hash and range key into the index, and ICLUDE projects only the keys specified in the non_key_attributes parameter. Required if has_global_secondary_index is true"
 #  default     = ""
 #}
